adb2ac21eeb6ef3f43274d41bf7596e5dd15b219
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-06-26 10:03:08 +0800

    add Maven code

---
+++ maven-project01/src/test/java/com/hitech/test/DemoTest.java
@@ -0,0 +1,20 @@
+package com.hitech.test;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+
+public class DemoTest {
+    Logger log;
+    @Test
+    public void test1() {
+        System.out.println("hello test1");;
+    }
+    @Test
+    public void test2() {
+        System.out.println("hello test2");;
+    }
+    @Test
+    public void test3() {
+        System.out.println("hello test3");;
+    }
+}
